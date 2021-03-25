75ea875e74d65f86a8b20537dff3c0efff189e81
b6320500549 <jirasin.po@ku.th>
2021-03-25 20:00:35 +0700

    Update 3.c

--- 3.c
+++ 3.c
@@ -2,9 +2,10 @@
 #include<string.h>
 int main()
 {
-   int n[3];
+
    int num,m,x,z,i=10,j=0;
    scanf("%d",&num);
+   int n[num];
    if(num>0&&num<10)
    {
        m=num;
@@ -19,6 +20,16 @@ int main()
            z=z/i;
            if(n[j]==z)
             m++;
+       }
+   }
+   else
+   {
+       for(x=100;x<=num;x++)
+       {
+           z=x;
+           n[j]=z%i;
+           z=z/i;
+
        }
    }
    printf("%d",m);

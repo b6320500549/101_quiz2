8eef16548fc1c8f0a682a21c28685356b68ab87e
b6320500549 <jirasin.po@ku.th>
2021-03-25 19:36:36 +0700

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,34 +1,49 @@
 #include<stdio.h>
 int main()
 {
-  int m,num,i,j=0,k=0,a;
+  int m=0,num,i,j,k,a,d,c=0,e;
   int s[100];
   scanf("%d",&num);
+  if(num>0&&num<=1000000)
+  {
+   for(d=1;d<=num;d++)
+  {
+   a=d;
+   j=0,k=0;
   for(i=0;i<100;i++)
   {
     s[i]=0;
   }
-  for(i=2;num!=1;)
+  for(i=2;a!=1;)
   {
-      if(num%i==0)
+      if(a%i==0)
       {
-      num=num/i;
+      a=a/i;
       s[j]=i;
       j++;
       }
       else
        i++;
   }
- int e = s[0];
- for(i=0;s[i]!=0;i++)
+  e=s[0];
+  for(i=0;s[i]!=0;i++)
   {
      if(s[i]==e)
          k++;
   }
   if(k>2)
-    printf("Not Cube Free");
+  {
+      if(d==num)
+      c++;
+  }
   else
-    printf("%d",num);
+      m++;
+  }
+  if(c>0)
+  printf("Not Cube Free");
+  else
+  printf("%d",m);
+  }
 
   return 0;
 }

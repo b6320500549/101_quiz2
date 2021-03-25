179646e68091a31869210e972023a6aaf90415de
b6320500549 <jirasin.po@ku.th>
2021-03-25 19:52:10 +0700

    Update 3.c

--- 3.c
+++ 3.c
@@ -2,7 +2,25 @@
 #include<string.h>
 int main()
 {
-   char n[3];
-   scanf("%3[^\n]s",n);
+   int n[3];
+   int num,m,x,z,i=10,j=0;
+   scanf("%d",&num);
+   if(num>0&&num<10)
+   {
+       m=num;
+   }
+   else if(num<100)
+   {
+       m=9;
+       for(x=10;x<=num;x++)
+       {
+           z=x;
+           n[j]=z%i;
+           z=z/i;
+           if(n[j]==z)
+            m++;
+       }
+   }
+   printf("%d",m);
    return 0;
 }

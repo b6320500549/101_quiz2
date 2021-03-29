d8b0ef3bead2d255e12c7d870005566cc7d4957f
b6320500549 <jirasin.po@ku.th>
2021-03-29 12:37:01 +0700

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,37 +1,27 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-
-   int num,m,x,z,i=10,j=0;
-   scanf("%d",&num);
-   int n[num];
-   if(num>0&&num<10)
-   {
-       m=num;
-   }
-   else if(num<100)
-   {
-       m=9;
-       for(x=10;x<=num;x++)
-       {
-           z=x;
-           n[j]=z%i;
-           z=z/i;
-           if(n[j]==z)
-            m++;
-       }
-   }
-   else
-   {
-       for(x=100;x<=num;x++)
-       {
-           z=x;
-           n[j]=z%i;
-           z=z/i;
-
-       }
-   }
-   printf("%d",m);
-   return 0;
+    long num,m,x,z,n[7],i=10,j=0,k=0,h=0,g=0;
+    scanf("%ld",&num);
+    if(num>0&&num<10)
+        m=num;
+    else
+    {
+        m=9;
+        for(x=10; x<=num; x++)
+        {
+            g=0; z=x;
+            for(j=0; z>0;  z=z/i,j++)
+                n[j]=z%i;
+            for(h=0; h<j/2; h++)
+            {
+                if(n[h]==n[(j-1)-h])
+                    g++;
+                if(g==j/2)
+                    m++;
+            }
+        }
+    }
+    printf("%ld",m);
+    return 0;
 }
